8852997d2e7479482c87d5d49cae72160079d026
Dylan Baker <dylanbaker7@gmail.com>
2026-04-10 18:38:16 -0400

    m

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
-import { nitro } from 'nitro/vite'
+
 
 const config = defineConfig({
   server: {
